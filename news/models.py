344ec459e2278057f9b217d881736bd577a14d35
Serhiy Smyslov <serhiy.smyslov@gmail.com>
2021-04-23 17:32:32 +0300

    Add debug-toolbar in project.

--- news/models.py
+++ news/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 
+
 class Category(models.Model):
     title = models.CharField(max_length=150, db_index=True, verbose_name='Название')
 
