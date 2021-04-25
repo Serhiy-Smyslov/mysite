354c458ae818803a18e722ae4e10e47584edcf06
Serhiy Smyslov <serhiy.smyslov@gmail.com>
2021-04-25 21:15:41 +0300

    Add Ckeditor in admin panel for News.

--- news/admin.py
+++ news/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
+from django import forms
 from django.utils.safestring import mark_safe
 
 from .models import News, Category
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class NewsAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = News
+        fields = '__all__'
 
 
 class NewsAdmin(admin.ModelAdmin):
+    form = NewsAdminForm
     list_display = ('id', 'title', 'created_at', 'is_published', 'get_image',)
     list_display_links = ('id', 'title')
     search_fields = ('title', 'created_at')
